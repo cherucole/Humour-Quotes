cad51863314d6a57cab956fcff4a4e63442f46db
cherucole <cheruyotcheruyot@gmail.com>
2018-08-09 17:05:59 +0300

    working quotes displayed

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-  constructor (public author:string, public quote:string, public publisher:string ){}
+  constructor (public author:string, public quote:string, public publisher:string, public timeDate:Date ){}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+quotes =[
+new Quote("cherucole", "my alarm is jealous of me and my bed", "cherucole", new Date(2018, 3,24)),
+new Quote("twitter", "opening my wallet makes me wanna cry", "twitter", new Date(2018, 3,24)),
+new Quote("cherucole", "my alarm is jealous of me and my bed", "cherucole", new Date(2018, 3,24)),
 
+]
   constructor() { }
 
   ngOnInit() {
