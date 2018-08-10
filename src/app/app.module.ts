faff75c4335176553fc0b0f0d9c1d9494383d51c
cherucole <cheruyotcheruyot@gmail.com>
2018-08-10 12:56:53 +0300

    page structure worked on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { TimeEllapsedPipe } from './time-ellapsed.pipe';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    TimeEllapsedPipe
+    TimeEllapsedPipe,
+    UserInputComponent
   ],
   imports: [
     BrowserModule
