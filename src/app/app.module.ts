5bc358ec03e4685290cdb26ff1e99dd3ef97080b
cherucole <cheruyotcheruyot@gmail.com>
2018-08-10 12:01:35 +0300

    added custom pipe for days after post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { TimeEllapsedPipe } from './time-ellapsed.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    TimeEllapsedPipe
   ],
   imports: [
     BrowserModule
