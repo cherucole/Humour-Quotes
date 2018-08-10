825ee298df3b83971068999902ac1b58b628997e
cherucole <cheruyotcheruyot@gmail.com>
2018-08-10 17:03:30 +0300

    added button logic emmit delete

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,12 +7,19 @@ import {Quote} from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+
 quotes =[
 new Quote("cherucole", "my alarm is jealous of me and my bed", "cherucole", new Date(2018, 3,24)),
 new Quote("twitter", "opening my wallet makes me wanna cry", "twitter", new Date(2018, 3,24)),
 new Quote("cherucole", "my alarm is jealous of me and my bed", "cherucole", new Date(2018, 3,24)),
 
 ]
+
+goalComplete(complete:boolean){
+  this.isComplete.emit(complete);
+}
+
   constructor() { }
 
   ngOnInit() {
